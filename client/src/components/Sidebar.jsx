78432cf8edc5a37a5abe427f16b9f1ff4ee4df8e
Ayush0095 <aayushsingh7668@gmail.com>
2025-08-17 14:21:15 +0530

    Components of HomePage

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Sidebar = ({selectedUser,setSelectedUser}) => {
+  return (
+    <div>
+      <div >
+        <h1>SideBar</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
