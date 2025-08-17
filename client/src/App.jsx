f530ce9fc6797a7356b9cc16d0341a62fd9c1432
Ayush0095 <aayushsingh7668@gmail.com>
2025-08-17 14:17:55 +0530

    Adding Pages Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import ProfilePage from './pages/ProfilePage'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
 
 
 const App = () => {
   return (
-    <div>
-      
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain" >
+      <Routes>
+        <Route path ="/" element={<HomePage/>}/>
+        <Route path ="/login" element={<LoginPage/>}/>
+        <Route path ="/profile" element={<ProfilePage/>}/>
+      </Routes>
     </div>
   )
 }
